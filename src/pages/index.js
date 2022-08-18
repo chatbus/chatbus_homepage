2faf8bbf3b5b2aa4f400900c51d171e7a20bfca6
JiamSeo <jams7777@gmail.com>
2022-08-18 21:14:08 +0900

    docker local env hot reload finish

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page1">
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
