ea2da3a28d09af573ec8897e8b95fc34b08a15ff
jams777 <jams7777@gmail.com>
2022-08-18 16:54:18 +0900

    github action deploy test

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
+    <Layout pageTitle="Home Page1">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
