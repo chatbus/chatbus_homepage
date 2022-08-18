4d611ac416d8f1c2cf7e6c1e7b8f53dded150b39
jams777 <jams7777@gmail.com>
2022-08-18 16:45:15 +0900

    gatsby minimal starter init

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/clifford.jpg"
+      />
+    </Layout>
+  )
+}
+
+export default IndexPage
