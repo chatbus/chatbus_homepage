ce413b43c6b0d15c88c0b1ae22bf0c6e9d28060c
jams777 <jams7777@gmail.com>
2022-09-19 09:56:21 +0900

    issue-21 #comments seo setting add

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,30 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My First Gatsby Site",
+    title: "Chatbus Homepage",
+    siteUrl: "https://chatbus.github.io",
+    description: "메시지 이동을 쉽게, 단순하게, 자유롭게~",
   },
   plugins: [
-    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://chatbus.github.io",
+        sitemap: "https://chatbus.github.io/sitemap/sitemap-index.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`${__dirname}/src/components/layout`),
+      },
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -14,12 +33,5 @@ module.exports = {
       }
     },
     "gatsby-plugin-mdx",
-    "gatsby-transformer-sharp",
-    {
-      resolve: `gatsby-plugin-layout`,
-      options: {
-        component: require.resolve(`${__dirname}/src/components/layout`),
-      },
-    },
   ],
 };
