a4110aa2a7d197d686bc706bab9140ef7e24efd9
chenchen <burub3@gmail.com>
2022-09-27 00:56:10 +0900

    merge 오류 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { graphql } from 'gatsby';
 import { motion } from "framer-motion";
 
-const IndexPage = () => {
+const IndexPage = ({data}) => {
     const title = data.mdx.frontmatter.title;
     const excerpt = data.mdx.excerpt;
     console.log("%c" +
@@ -23,7 +23,7 @@ const IndexPage = () => {
                 exit={{ opacity: 0 }}
                 transition={{ duration: 0.50 }}
             >
-            <h2 className="mb-8 text-3xl font-bold leading-snug text-dark sm:text-4xl sm:leading-snug md:text-[45px] md:leading-snug md:pt-[50px] lg:pt-[100px]">프로젝트 소개/h2>
+            <h2 className="mb-8 text-3xl font-bold leading-snug text-dark sm:text-4xl sm:leading-snug md:text-[45px] md:leading-snug md:pt-[50px] lg:pt-[100px]">프로젝트 소개</h2>
             <p className="text-lg leading-relaxed text-body-color sm:text-xl sm:leading-relaxed">{title}</p>
 
             <div className="-mx-4 flex flex-wrap mt-[60px]">
